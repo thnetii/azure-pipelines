24b821cebfe0555e8ad403889c8fc9ad73401d65
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-06-03 14:12:30 +0200

    Fix line endings and remove UTF-8 BOM

--- src/THNETII.AzureDevOps.Pipelines.Logging/VsoNullLoggingScope.cs
+++ src/THNETII.AzureDevOps.Pipelines.Logging/VsoNullLoggingScope.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace THNETII.AzureDevOps.Pipelines.Logging
 {
--- src/THNETII.AzureDevOps.Pipelines.VstsTaskSdk/VstsArtifactType.cs
+++ src/THNETII.AzureDevOps.Pipelines.VstsTaskSdk/VstsArtifactType.cs
@@ -1,4 +1,4 @@
-﻿using System.Runtime.Serialization;
+using System.Runtime.Serialization;
 
 namespace THNETII.AzureDevOps.Pipelines.VstsTaskSdk
 {
