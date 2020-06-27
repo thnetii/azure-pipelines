93754a66e8105597cb053f0d230884f15b61bc0d
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-06-27 16:42:44 +0200

    Create new YAML evaluator project

---
+++ src/THNETII.AzureDevOps.Pipelines.YamlEval/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace THNETII.AzureDevOps.Pipelines.YamlEval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
