607e999a565ef310d568c2665e39ee0d2a016b6c
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-08-08 17:36:10 +0200

    Use Expression to output job steps

--- templates/playground/generate.ps1
+++ templates/playground/generate.ps1
@@ -1,13 +1,10 @@
-$Matrix = [PSCustomObject]@{ }
-1..5 | ForEach-Object {
-    $Row = [PSCustomObject]@{
-        "Playground.GeneratedJob.Echo" = "Output from Matrix row $_";
-        "Playground.GeneratedJob.DisplayName" = "Row $_";
-        "Playground.GeneratedJob.TaskDisplayName" = "Matrix Row $_ Output";
+$StepItems = 1..5 | ForEach-Object {
+    [PSCustomObject]@{
+        pwsh = "Write-Host `"Output from Step $_`"";
+        displayName = "Generated Step $_"
     }
-    $Matrix | Add-Member NoteProperty $_ $Row
 }
-$ValueString = $Matrix | ConvertTo-Json -Compress
+$ValueString = $StepItems | ConvertTo-Json -Compress
 $LoggingCommandFile = Resolve-Path -ErrorAction "Continue" $ENV:LoggingCommandFunctions
 if (-not $LoggingCommandFile) {
     $LoggingCommandFile = ([System.IO.Path]::Combine(
@@ -19,7 +16,7 @@ if (-not $LoggingCommandFile) {
 . $LoggingCommandFile
 $command = Write-LoggingCommand -Area 'task' -Event 'setvariable' `
     -Data $ValueString -Properties @{
-        'variable' = "legs";
+        'variable' = "items";
         'isOutput' = "true";
     } -AsOutput
 Write-Host $command
