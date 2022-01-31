f9788f670515782fe040ccfc606ebd5aa21c7691
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2022-01-31 10:57:16 +0100

    Apply double quoted strings rule

--- src/azure-pipelines-vso-tsc/tsc.mjs
+++ src/azure-pipelines-vso-tsc/tsc.mjs
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
-import path from 'path';
-import { command, getVariable } from 'azure-pipelines-task-lib';
-import { _writeLine } from 'azure-pipelines-task-lib/internal.js';
-import { ToolRunner } from 'azure-pipelines-task-lib/toolrunner.js';
+import path from "path";
+import { command, getVariable } from "azure-pipelines-task-lib";
+import { _writeLine } from "azure-pipelines-task-lib/internal.js";
+import { ToolRunner } from "azure-pipelines-task-lib/toolrunner.js";
 const argv = process.argv.slice(2);
 
-const sourcesRootDirectory = getVariable('Build.SourcesDirectory')
-  || process.env['Build.SourcesDirectory'] || path.resolve();
+const sourcesRootDirectory = getVariable("Build.SourcesDirectory")
+  || process.env["Build.SourcesDirectory"] || path.resolve();
 
-const tscRunner = new ToolRunner('npx');
-tscRunner.arg('tsc');
+const tscRunner = new ToolRunner("npx");
+tscRunner.arg("tsc");
 tscRunner.arg(argv);
 
 /** @see https://github.com/microsoft/vscode/blob/7db1a2b88f7557e0a43fec75b6ba7e50b3e9f77e/extensions/typescript-language-features/package.json#L1296 */
-const tscRegexPattern = '^([^\\s].*)[\\(:](\\d+)[,:](\\d+)(?:\\):\\s+|\\s+-\\s+)(error|warning|info)\\s+TS(\\d+)\\s*:\\s*(.*)$';
-const tscRegexMatcher = new RegExp(tscRegexPattern, 'u');
+const tscRegexPattern = "^([^\\s].*)[\\(:](\\d+)[,:](\\d+)(?:\\):\\s+|\\s+-\\s+)(error|warning|info)\\s+TS(\\d+)\\s*:\\s*(.*)$";
+const tscRegexMatcher = new RegExp(tscRegexPattern, "u");
 
-tscRunner.on('stdline', /** @param {string} line */ line => {
+tscRunner.on("stdline", /** @param {string} line */ line => {
   _writeLine(line);
   const tscOutputMatch = tscRegexMatcher.exec(line);
   if (!tscOutputMatch) {
@@ -30,7 +30,7 @@ tscRunner.on('stdline', /** @param {string} line */ line => {
     columnnumber: column ? parseInt(column, 10) : undefined,
     code
   };
-  command('task.logissue', tscCmdProps, message);
+  command("task.logissue", tscCmdProps, message);
 });
 
 tscRunner.exec().then(exitCode => {
