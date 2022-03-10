17be0134ce7073e6fba1b3469006c5cf6f2ed58c
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2022-03-10 11:12:42 +0100

    Use single-quotes

--- src/azure-pipelines-vso-eslint/eslint.mjs
+++ src/azure-pipelines-vso-eslint/eslint.mjs
@@ -1,25 +1,25 @@
 #!/usr/bin/env node
-import path from "path";
-import { command, getVariable } from "azure-pipelines-task-lib";
-import { _writeLine } from "azure-pipelines-task-lib/internal.js";
-import { ToolRunner } from "azure-pipelines-task-lib/toolrunner.js";
+import path from 'path';
+import { command, getVariable } from 'azure-pipelines-task-lib';
+import { _writeLine } from 'azure-pipelines-task-lib/internal.js';
+import { ToolRunner } from 'azure-pipelines-task-lib/toolrunner.js';
 const argv = process.argv.slice(2);
 
-const sourcesRootDirectory = getVariable("Build.SourcesDirectory")
-  || process.env["Build.SourcesDirectory"] || path.resolve();
+const sourcesRootDirectory = getVariable('Build.SourcesDirectory')
+  || process.env['Build.SourcesDirectory'] || path.resolve();
 
 const eslintTracker = {
   warningCount: 0,
   errorCount: 0
 };
-const eslintRunner = new ToolRunner("npx");
-eslintRunner.arg("eslint");
+const eslintRunner = new ToolRunner('npx');
+eslintRunner.arg('eslint');
 eslintRunner.arg(argv);
 
 /** @see https://github.com/microsoft/vscode/blob/7db1a2b88f7557e0a43fec75b6ba7e50b3e9f77e/src/vs/workbench/contrib/tasks/common/problemMatcher.ts#L1285-L1294 */
 const eslintRegexMatcher = /^(.+):\sline\s(\d+),\scol\s(\d+),\s(Error|Warning|Info)\s-\s(.+)\s\((.+)\)$/u;
 
-eslintRunner.on("stdline", /** @param {string} line */ line => {
+eslintRunner.on('stdline', /** @param {string} line */ line => {
   _writeLine(line);
   const eslintOutputMatch = eslintRegexMatcher.exec(line);
   if (!eslintOutputMatch) {
@@ -35,15 +35,14 @@ eslintRunner.on("stdline", /** @param {string} line */ line => {
   };
   if (/^warning$/ui.test(severity)) {
     eslintTracker.warningCount += 1;
-  }
-  else if (/^error$/ui.test(severity)) {
+  } else if (/^error$/ui.test(severity)) {
     eslintTracker.errorCount += 1;
   }
-  command("task.logissue", logCmdProps, message);
+  command('task.logissue', logCmdProps, message);
 });
 
 eslintRunner.exec().then(exitCode => {
-  command("task.complete", {
+  command('task.complete', {
     result: eslintTracker.errorCount ? 'Failed' : eslintTracker.warningCount
       ? 'SucceededWithIssues' : 'Succeeded'
   }, `ESLint exited with code: ${exitCode}`);
--- src/azure-pipelines-vso-tsc/tsc.mjs
+++ src/azure-pipelines-vso-tsc/tsc.mjs
@@ -1,26 +1,26 @@
 #!/usr/bin/env node
-import path from "path";
-import { command, getVariable } from "azure-pipelines-task-lib";
-import { _writeLine } from "azure-pipelines-task-lib/internal.js";
-import { ToolRunner } from "azure-pipelines-task-lib/toolrunner.js";
+import path from 'path';
+import { command, getVariable } from 'azure-pipelines-task-lib';
+import { _writeLine } from 'azure-pipelines-task-lib/internal.js';
+import { ToolRunner } from 'azure-pipelines-task-lib/toolrunner.js';
 const argv = process.argv.slice(2);
 
-const sourcesRootDirectory = getVariable("Build.SourcesDirectory")
-  || process.env["Build.SourcesDirectory"] || path.resolve();
+const sourcesRootDirectory = getVariable('Build.SourcesDirectory')
+  || process.env['Build.SourcesDirectory'] || path.resolve();
 
 const tscTracker = {
   warningCount: 0,
   errorCount: 0
 };
-const tscRunner = new ToolRunner("npx");
-tscRunner.arg("tsc");
+const tscRunner = new ToolRunner('npx');
+tscRunner.arg('tsc');
 tscRunner.arg(argv);
 
 /** @see https://github.com/microsoft/vscode/blob/7db1a2b88f7557e0a43fec75b6ba7e50b3e9f77e/extensions/typescript-language-features/package.json#L1296 */
-const tscRegexPattern = "^([^\\s].*)[\\(:](\\d+)[,:](\\d+)(?:\\):\\s+|\\s+-\\s+)(error|warning|info)\\s+TS(\\d+)\\s*:\\s*(.*)$";
-const tscRegexMatcher = new RegExp(tscRegexPattern, "u");
+const tscRegexPattern = '^([^\\s].*)[\\(:](\\d+)[,:](\\d+)(?:\\):\\s+|\\s+-\\s+)(error|warning|info)\\s+TS(\\d+)\\s*:\\s*(.*)$';
+const tscRegexMatcher = new RegExp(tscRegexPattern, 'u');
 
-tscRunner.on("stdline", /** @param {string} line */ line => {
+tscRunner.on('stdline', /** @param {string} line */ line => {
   const tscOutputMatch = tscRegexMatcher.exec(line);
   if (!tscOutputMatch) {
     return;
@@ -35,15 +35,14 @@ tscRunner.on("stdline", /** @param {string} line */ line => {
   };
   if (/^warning$/ui.test(severity)) {
     tscTracker.warningCount += 1;
-  }
-  else if (/^error$/ui.test(severity)) {
+  } else if (/^error$/ui.test(severity)) {
     tscTracker.errorCount += 1;
   }
-  command("task.logissue", tscCmdProps, message);
+  command('task.logissue', tscCmdProps, message);
 });
 
 tscRunner.exec().then(exitCode => {
-  command("task.complete", {
+  command('task.complete', {
     result: tscTracker.errorCount ? 'Failed' : tscTracker.warningCount
       ? 'SucceededWithIssues' : 'Succeeded'
   }, `TSC exited with code: ${exitCode}`);
