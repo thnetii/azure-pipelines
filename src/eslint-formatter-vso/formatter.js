504db13ad965635966ac2c3bddce6a61b76881fe
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2022-03-24 14:25:51 +0100

    Create ESLint formatter using azure-pipelines-task-lib

--- src/eslint-formatter-vso/formatter.js
+++ src/eslint-formatter-vso/formatter.js
@@ -1,6 +1,39 @@
+const path = require('path');
+const process = require('process');
+const { getVariable, command, setResult, TaskResult, IssueType } = require('azure-pipelines-task-lib');
+const { logIssue } = require('azure-pipelines-task-lib/task');
+
+/** @type {{[s in import('eslint').Linter.Severity]: IssueType}} */
+const issueType = {
+  0: IssueType.Warning,
+  1: IssueType.Warning,
+  2: IssueType.Error
+};
+
 /** @type {import('eslint').ESLint.Formatter} */
 const formatter = {
   format(results) {
+    const sourcesRootDirectory = getVariable('Build.SourcesDirectory')
+      || process.env['Build.SourcesDirectory'] || path.resolve();
+    const totalCount = { warning: 0, error: 0, fatal: 0 };
+    for (const { filePath, messages, errorCount, fatalErrorCount, warningCount } of results) {
+      const relPath = path.relative(filePath, sourcesRootDirectory);
+      for (const { line, column, severity, message, ruleId } of messages) {
+        logIssue(
+          issueType[severity], message,
+          relPath, line, column, ruleId === null ? undefined : ruleId
+        );
+      }
+      totalCount.warning += warningCount;
+      totalCount.error += errorCount;
+      totalCount.fatal += fatalErrorCount;
+    }
+    let result = TaskResult.Succeeded;
+    if (totalCount.warning)
+      result = TaskResult.SucceededWithIssues;
+    if (totalCount.error || totalCount.fatal)
+      result = TaskResult.Failed;
+    setResult(result, '');
   },
 };
 
