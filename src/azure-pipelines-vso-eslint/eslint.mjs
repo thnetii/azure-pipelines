d86bae7fbffe5ae59cd51bad88488ae593acdde4
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2022-03-24 10:00:11 +0100

    Run ESLint --fix

--- src/azure-pipelines-vso-eslint/eslint.mjs
+++ src/azure-pipelines-vso-eslint/eslint.mjs
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 import path from 'path';
 import {
-  command, debug, getVariable, setResult, TaskResult
+  command, debug, getVariable, setResult, TaskResult,
 } from 'azure-pipelines-task-lib';
-import { _writeLine } from 'azure-pipelines-task-lib/internal.js';
-import { ToolRunner } from 'azure-pipelines-task-lib/toolrunner.js';
+import { _writeLine } from 'azure-pipelines-task-lib/internal';
+import { ToolRunner } from 'azure-pipelines-task-lib/toolrunner';
+
 const argv = process.argv.slice(2);
 
 const sourcesRootDirectory = getVariable('Build.SourcesDirectory')
@@ -12,7 +13,7 @@ const sourcesRootDirectory = getVariable('Build.SourcesDirectory')
 
 const runTracker = {
   warningCount: 0,
-  errorCount: 0
+  errorCount: 0,
 };
 const eslintRunner = new ToolRunner('npx');
 eslintRunner.arg('eslint');
@@ -21,7 +22,7 @@ eslintRunner.arg(argv);
 /** @see https://github.com/microsoft/vscode/blob/7db1a2b88f7557e0a43fec75b6ba7e50b3e9f77e/src/vs/workbench/contrib/tasks/common/problemMatcher.ts#L1285-L1294 */
 const eslintRegexMatcher = /^(.+):\sline\s(\d+),\scol\s(\d+),\s(Error|Warning|Info)\s-\s(.+)\s\((.+)\)$/u;
 
-eslintRunner.on('stdline', /** @param {string} line */ line => {
+eslintRunner.on('stdline', /** @param {string} line */ (line) => {
   _writeLine(line);
   const eslintOutputMatch = eslintRegexMatcher.exec(line);
   if (!eslintOutputMatch) {
@@ -33,7 +34,7 @@ eslintRunner.on('stdline', /** @param {string} line */ line => {
     type: severity,
     linenumber: lineno ? parseInt(lineno, 10) : undefined,
     columnnumber: column ? parseInt(column, 10) : undefined,
-    code
+    code,
   };
   if (/^warning$/ui.test(severity)) {
     runTracker.warningCount += 1;
@@ -43,7 +44,7 @@ eslintRunner.on('stdline', /** @param {string} line */ line => {
   command('task.logissue', logCmdProps, message);
 });
 
-eslintRunner.exec({ ignoreReturnCode: true }).then(exitCode => {
+eslintRunner.exec({ ignoreReturnCode: true }).then((exitCode) => {
   let result = TaskResult.Succeeded;
   if (runTracker.warningCount) {
     result = TaskResult.SucceededWithIssues;
