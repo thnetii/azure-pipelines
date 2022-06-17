dd59951f7f8214b3b06fdb55a152616c5a8a653d
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2022-06-17 16:16:36 +0200

    Buffer logIssue commands until tool has finished

--- src/azure-pipelines-vso-npm/vso-npm.js
+++ src/azure-pipelines-vso-npm/vso-npm.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+const os = require('os');
 const {
   command, debug, setResult, TaskResult,
 } = require('azure-pipelines-task-lib');
@@ -45,20 +46,105 @@ const tryGetWarningMatch = (line, onMatch) => {
   return true;
 };
 
+/** @type {{props: Object, message: string}[]} */
+const logIssueBuffer = [];
 /** @type {onMatchCallback} */
-const logIssue = (props, message) => {
-  command('task.logissue', props, message);
+const pushLogIssue = (props, message) => {
+  logIssueBuffer.push({ props, message });
 };
 
-toolRunner.on('errline', /** @param {string} line */(line) => {
-  if (tryGetErrorMatch(line, logIssue)) {
+/** @param {string} line */
+const onStdErrLine = (line) => {
+  if (tryGetErrorMatch(line, pushLogIssue)) {
     runTracker.errorCount += 1;
-  } else if (tryGetWarningMatch(line, logIssue)) {
+  } else if (tryGetWarningMatch(line, pushLogIssue)) {
     runTracker.warningCount += 1;
+  }
+};
+
+/**
+ * @typedef {Object} IndexOfAnyNoMatchInfo
+ * @property {false} found
+ * @property {-1} idx
+ */
+
+/**
+ * @typedef {Object} IndexOfAnyFoundMatchInfo
+ * @property {true} found
+ * @property {number} idx
+ * @property {string} match
+ */
+
+/**
+ * @typedef {IndexOfAnyNoMatchInfo | IndexOfAnyFoundMatchInfo} IndexOfAnyInfo
+ */
+
+/**
+ * @param {string} str
+ * @param {string[]} search
+ */
+const getIndexOfAnyInfo = (str, search) => {
+  /** @type {IndexOfAnyInfo} */
+  let result = {
+    found: false,
+    idx: -1,
+  };
+
+  /**
+   * @param {IndexOfAnyInfo} r1
+   * @param {IndexOfAnyFoundMatchInfo} r2
+   */
+  const getBetterResult = (r1, r2) => {
+    if (!r1.found) { return r2; }
+    if (r1.idx < r2.idx) { return r1; }
+    if (r1.idx > r2.idx) { return r2; }
+    if (r1.match.length > r2.match.length) { return r1; }
+    return r2;
+  };
+
+  for (const srch of search) {
+    const idx = str.indexOf(srch);
+    if (idx >= 0) {
+      result = getBetterResult(result, {
+        found: true,
+        idx,
+        match: srch,
+      });
+    }
+  }
+  return result;
+};
+
+let stderrBuffer = '';
+toolRunner.on('stderr', /** @param {Buffer} data */(data) => {
+  /** @see https://docs.microsoft.com/en-us/dotnet/api/system.io.textreader.readline?view=net-6.0#remarks */
+  const eol = ['\r', '\n', '\r\n', os.EOL];
+  try {
+    let s = stderrBuffer + data.toString();
+    let idxInfo = getIndexOfAnyInfo(s, eol);
+    while (idxInfo.found) {
+      const line = s.substring(0, idxInfo.idx);
+      onStdErrLine(line);
+      // the rest of the string ...
+      s = s.substring(idxInfo.idx + idxInfo.match.length);
+      idxInfo = getIndexOfAnyInfo(s, eol);
+    }
+    stderrBuffer = s;
+  } catch (err) {
+    // streaming lines to console is best effort.  Don't fail a build.
+    debug('error processing line');
   }
 });
 
 toolRunner.exec({ ignoreReturnCode: true }).then((exitCode) => {
+  if (stderrBuffer) {
+    onStdErrLine(stderrBuffer);
+  }
+  return exitCode;
+}).then((exitCode) => {
+  for (const { props, message } of logIssueBuffer) {
+    command('task.logissue', props, message);
+  }
   let result = TaskResult.Succeeded;
   if (runTracker.warningCount) {
     result = TaskResult.SucceededWithIssues;
