f5e17965a476382b0439ac93286fbf360f3cabc1
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2022-08-09 14:04:13 +0200

    Simplify output line reading by using node.js readline

--- src/azure-pipelines-vso-npm/vso-npm.js
+++ src/azure-pipelines-vso-npm/vso-npm.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-const os = require('os');
+const { PassThrough } = require('stream');
+const readline = require('readline');
 const {
   command,
   debug,
@@ -62,91 +63,26 @@ const pushLogIssue = (props, message) => {
   logIssueBuffer.push({ props, message });
 };
 
-/** @param {string} line */
-const onStdErrLine = (line) => {
+const errlinePassThrough = new PassThrough();
+const errline = readline.createInterface({
+  input: errlinePassThrough,
+  crlfDelay: Infinity,
+});
+errline.on('line', (line) => {
   if (tryGetErrorMatch(line, pushLogIssue)) {
     runTracker.errorCount += 1;
   } else if (tryGetWarningMatch(line, pushLogIssue)) {
     runTracker.warningCount += 1;
   }
-};
+});
 
-/**
- * @typedef {Object} IndexOfAnyNoMatchInfo
- * @property {false} found
- * @property {-1} idx
- */
-
-/**
- * @typedef {Object} IndexOfAnyFoundMatchInfo
- * @property {true} found
- * @property {number} idx
- * @property {string} match
- */
-
-/**
- * @typedef {IndexOfAnyNoMatchInfo | IndexOfAnyFoundMatchInfo} IndexOfAnyInfo
- */
-
-/**
- * @param {string} str
- * @param {string[]} search
- */
-const getIndexOfAnyInfo = (str, search) => {
-  /** @type {IndexOfAnyInfo} */
-  let result = {
-    found: false,
-    idx: -1,
-  };
-
-  /**
-   * @param {IndexOfAnyInfo} r1
-   * @param {IndexOfAnyFoundMatchInfo} r2
-   */
-  const getBetterResult = (r1, r2) => {
-    if (!r1.found) return r2;
-    if (r1.idx < r2.idx) return r1;
-    if (r1.idx > r2.idx) return r2;
-    if (r1.match.length > r2.match.length) return r1;
-    return r2;
-  };
-
-  for (const srch of search) {
-    const idx = str.indexOf(srch);
-    if (idx >= 0) {
-      result = getBetterResult(result, {
-        found: true,
-        idx,
-        match: srch,
-      });
-    }
-  }
-  return result;
-};
-
-let stderrBuffer = '';
 toolRunner.on(
   'stderr',
   /** @param {Buffer} data */ (data) => {
-    /** @see https://docs.microsoft.com/en-us/dotnet/api/system.io.textreader.readline?view=net-6.0#remarks */
-    const eol = ['\r', '\n', '\r\n', os.EOL];
-    try {
-      let s = stderrBuffer + data.toString();
-      let idxInfo = getIndexOfAnyInfo(s, eol);
-      while (idxInfo.found) {
-        const line = s.substring(0, idxInfo.idx);
-        onStdErrLine(line);
-        // the rest of the string ...
-        s = s.substring(idxInfo.idx + idxInfo.match.length);
-        idxInfo = getIndexOfAnyInfo(s, eol);
-      }
-      stderrBuffer = s;
-    } catch (err) {
-      // streaming lines to console is best effort.  Don't fail a build.
-      debug('error processing line');
-    }
+    errlinePassThrough.write(data);
   }
 );
+toolRunner.on('done', () => errlinePassThrough.end());
 
 /** @see https://github.com/dword-design/package-name-regex/blob/658ce7a661512f3e1e5496d6eb1dfd5ec8ae65a1/src/index.js */
 const npmPackageNameRegex =
@@ -156,61 +92,53 @@ const npmPackageNameRegexWithVersionAndColon = new RegExp(
   'u'
 );
 
-toolRunner
-  .exec({ ignoreReturnCode: true })
-  .then((exitCode) => {
-    if (stderrBuffer) {
-      onStdErrLine(stderrBuffer);
-    }
-    return exitCode;
-  })
-  .then((exitCode) => {
-    const mergedLogIssueBuffer = logIssueBuffer.reduce((acc, curr) => {
-      const prv = acc[acc.length - 1];
-      let merged = false;
-      if (prv) {
-        const {
-          props: { type: prvType, code: prvCode },
-          message: prvMsg,
-        } = prv;
-        const {
-          props: { type, code },
-          message,
-        } = curr;
+toolRunner.exec({ ignoreReturnCode: true }).then((exitCode) => {
+  const mergedLogIssueBuffer = logIssueBuffer.reduce((acc, curr) => {
+    const prv = acc[acc.length - 1];
+    let merged = false;
+    if (prv) {
+      const {
+        props: { type: prvType, code: prvCode },
+        message: prvMsg,
+      } = prv;
+      const {
+        props: { type, code },
+        message,
+      } = curr;
+      if (
+        prvType === type &&
+        prvCode &&
+        code &&
+        prvCode === code &&
+        prvMsg.startsWith(prvCode) &&
+        message.startsWith(code)
+      ) {
+        const prvReason = prvMsg.substring(prvCode.length);
+        const reason = message.substring(code.length);
         if (
-          prvType === type &&
-          prvCode &&
-          code &&
-          prvCode === code &&
-          prvMsg.startsWith(prvCode) &&
-          message.startsWith(code)
+          npmPackageNameRegexWithVersionAndColon.test(prvReason) &&
+          !npmPackageNameRegexWithVersionAndColon.test(reason)
         ) {
-          const prvReason = prvMsg.substring(prvCode.length);
-          const reason = message.substring(code.length);
-          if (
-            npmPackageNameRegexWithVersionAndColon.test(prvReason) &&
-            !npmPackageNameRegexWithVersionAndColon.test(reason)
-          ) {
-            prv.message += `\n${reason}`;
-            merged = true;
-          }
+          prv.message += `\n${reason}`;
+          merged = true;
         }
       }
-      if (!merged) {
-        acc.push(curr);
-      }
-      return acc;
-    }, /** @type {typeof logIssueBuffer} */ ([]));
-    for (const { props, message } of mergedLogIssueBuffer) {
-      command('task.logissue', props, message);
     }
-    let result = TaskResult.Succeeded;
-    if (runTracker.warningCount) {
-      result = TaskResult.SucceededWithIssues;
+    if (!merged) {
+      acc.push(curr);
     }
-    if (runTracker.errorCount) {
-      result = TaskResult.Failed;
-    }
-    setResult(result, '');
-    debug(`npm exited with code '${exitCode}'.`);
-  });
+    return acc;
+  }, /** @type {typeof logIssueBuffer} */ ([]));
+  for (const { props, message } of mergedLogIssueBuffer) {
+    command('task.logissue', props, message);
+  }
+  let result = TaskResult.Succeeded;
+  if (runTracker.warningCount) {
+    result = TaskResult.SucceededWithIssues;
+  }
+  if (runTracker.errorCount) {
+    result = TaskResult.Failed;
+  }
+  setResult(result, '');
+  debug(`npm exited with code '${exitCode}'.`);
+});
